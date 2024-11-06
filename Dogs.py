a2abc67f89b03cfddab2330b54b91f1190cb0fd4
Sartlyan <shadrinaspb@gmail.com>
2024-11-06 16:37:14 +0300

    Создана функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -4,14 +4,14 @@ from tkinter import messagebox as mb
 from PIL import Image, ImageTk
 from io import BytesIO
 
-# def get_random_dog_image():
-  #  try:
-   #     response = requests.get('https://dog.ceo/api/breeds/image/random')
-    #    response.raise_for_status()
-     #   data = response.json()
-      #  return data['message']
-   # except Exception as e:
-    #    messagebox.showerror("Ошибка", f"Ошибка при запросе к API: {e}")
+def get_random_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data['message']
+    except Exception as e:
+        mb.showerror("Ошибка", f"Ошибка при запросе к API: {e}")
      #   return None
 
 def show_image():
@@ -23,12 +23,14 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
-          #  img = ImageTk.PhotoImage(img)
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
 
         except requests.RequestException as e:
-            mb.showerror("Ошибка", f"Возникла ошибка: {e}")
+            mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
+            return None
+
 
 window = Tk()
 window.title("Картинки с собачками")
